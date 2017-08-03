bae9d70d08f01042c12c333be97c15b582c47916
Kayla-Jade Butkow <kaylabutkow@gmail.com>
2017-08-03 14:45:15 +0200

    Exercise 4.1

--- screen/scr_main.cpp
+++ screen/scr_main.cpp
@@ -7,7 +7,37 @@ using namespace std;
 int main()
 {
 	Screen myScreen{6,6};
-
+	
+	// creating a 'K'
+	myScreen.clear(' ');
+	myScreen.set('*');
+	myScreen.down();
+	myScreen.set('*');
+	myScreen.move(1,6);
+	myScreen.set('*');
+	myScreen.move(2,4);
+	myScreen.set('*');
+	myScreen.move(3,2);
+	myScreen.set('*');
+	myScreen.back();
+	myScreen.set('*');
+	myScreen.down();
+	myScreen.set('*');
+	myScreen.down();
+	myScreen.set('*');
+	myScreen.down();
+	myScreen.set('*');
+	myScreen.move(4,2);
+	myScreen.set('*');
+	myScreen.move(5,4);
+	myScreen.set('*');
+	myScreen.move(6,6);
+	myScreen.set('*');
+	myScreen.display();
+	
+	cout << endl;
+	
+	myScreen.clear('#');
 	myScreen.forward();
 	myScreen.set('*');
 	myScreen.down();
@@ -16,18 +46,18 @@ int main()
 	myScreen.set("---");
 
 	myScreen.display();
+	
 	cout << endl;
 
 	myScreen.reSize(16,16);
 	myScreen.display();
 	myScreen.clear(' ');
-
 	myScreen.move(7,7);
 	myScreen.set("BIG");
 	myScreen.move(8,5);
 	myScreen.set("SCREEN");
 	myScreen.display();
-
+	
 	return 0;
 }
 
