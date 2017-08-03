8953b80f9c610786baf7eef215fbcd45e09bf355
Kayla-Jade Butkow <kaylabutkow@gmail.com>
2017-08-03 14:30:32 +0200

    Exercise 3.1

--- complex/complex.cpp
+++ complex/complex.cpp
@@ -10,6 +10,7 @@ int main()
 {
     complex<float> num1{ 2.0, 2.0 };  // using C++11 uniform initialisation syntax
     complex<float> num2{ 4.0, -2.0 }; // old syntax: complex<float> num2(4.0,-2.0);
+	
 
     auto answer = num1 * num2; // using C++11 auto keyword,
                                // answer is of type: complex<float>
@@ -17,7 +18,11 @@ int main()
     cout << "The answer is: " << answer << endl;
     cout << "Or in a more familiar form: " << answer.real() << " + " << answer.imag() << "j" << endl;
 
-    // answer++;
+	// answer++; 
+	//answer is a complex number. A complex number is made up of real(.real()) and imaginary(.imag()) parts
+	//It is effectively a variable that stores numbers in two vector elements.
+	//If one merely attempts to increment the variable answer by 1, it is not known which element should be incremented
+	//Instead, the code should specify whether the imaginary part or the real part needs to be incremented.
 
     return 0;
 }
